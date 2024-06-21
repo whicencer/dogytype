123422eabedacea9aa93e6fe9313ef4e29bbd456
Deniel <denielaz2020@gmail.com>
2024-06-21 13:53:41 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1>Dogytype</h1>
+</template>
+
+<style scoped>
+  h1 {
+    color: rgb(224, 179, 54);
+    font-weight: 500;
+  }
+  .sum {
+    margin-top: 10px;
+  }
+</style>
